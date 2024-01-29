240d5a739afdee8d6201a01a09c5cbac915e3e38
arjun <arjun.betageri@live.com>
2024-01-30 03:07:44 +0530

    v7  GET

--- Lambda_functions/anymail/lambda_function.py
+++ Lambda_functions/anymail/lambda_function.py
@@ -6,7 +6,7 @@ def lambda_handler(event, context):
 
     response = {
         "statusCode": 200,
-        "body": json.dumps({"message": "GET v14 request processed successfully"}),
+        "body": json.dumps({"message": "GET v15 request processed successfully"}),
     }
 
     return response
